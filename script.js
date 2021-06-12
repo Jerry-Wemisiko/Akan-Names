78dda9bf52be4680e7968f498776ea64adb5bb39
Jerry-Wemisiko <jerry.wemisiko@student.moringaschool.com>
2021-06-12 23:28:44 +0300

    create a form

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
 // Enter your birthday section
     //An array for days;
-    var days =["Monday","Tuesday","Wednesday","Thursday","Friday"];
+function birthday(){
+   var days =["Monday","Tuesday","Wednesday","Thursday","Friday"];
 
+    var months=["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+    if (days<0 || days>31){
+        alert("Invalid")
+    }
+    if (months<0||months>12){
+        alert("Invalid")
+    }
+
+
+
+}
+
+
+
+
+
+    var days =["Monday","Tuesday","Wednesday","Thursday","Friday"];
     //An array for months
     var months=["January","February","March","April","May","June","July","August","September","October","November","December"];
-//Gender section
+     //operators 
+    if (days<0 || days>31){
+        alert("Invalid")
+    }
+
+    if (months<0||months>12){
+        alert("Invalid")
+    }
+    //function to calculate day of the week one was born
+
+// //Gender section
 //Button to display Akan name 
