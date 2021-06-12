a8ca41bc49f63e2ab96045b6800eb5bcec69bc91
Jerry-Wemisiko <jerry.wemisiko@student.moringaschool.com>
2021-06-12 22:35:03 +0300

    create days and months arrays

---
+++ script.js
@@ -0,0 +1,8 @@
+// Enter your birthday section
+    //An array for days;
+    var days =["Monday","Tuesday","Wednesday","Thursday","Friday"];
+
+    //An array for months
+    var months=["January","February","March","April","May","June","July","August","September","October","November","December"];
+//Gender section
+//Button to display Akan name 
