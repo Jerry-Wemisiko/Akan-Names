4c592d8059395c1f4035b13529d4687c64a73318
Jerry-Wemisiko <jerry.wemisiko@student.moringaschool.com>
2021-06-13 20:00:25 +0300

    styled the form

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-//Form validation
-function validateForm(){
-    var day = document.myForm.day.value;
-    var month =document.myForm.month.value;
-    var year =document.myForm.year.value;
+// // use arrays to represent day and name one was born
 
-    if (day<0 || day>31){
-        alert("Day is invalid");
-        return false;
-    }else if(month<0 ||month>12){
-        alert("Month is invalid");
-        return false;
-    }
+// // Form validation
+// function computeForm(){
+//     var day = document.myForm.day.value;
+//     var month =document.myForm.month.value;
+//     var year =document.myForm.year.value;
 
-    if(year<1900){
-        alert("Extremely old");
-        return false;
-    }
-}
+// if (day<0 || day>31 || month<0 || month>12 ||year<1900){
+//     alert("Enter valid Input");
+//     // return false;
+// }else {
+//         var day = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+//         var month =["January","February","March","April","May","June","July","August","September","October","November","December"];
+//         var currentTime=new Date(parseInt(day[0]),parseInt(month),parseInt(year));
+//         var currentDay =currentTime.getDay();
+//         var currentDayName =day[currentDay];
+//         document.write("You were born on"+ currentDayName);
+//         document.getElementById('demo')
+
+// }
+// }
 
